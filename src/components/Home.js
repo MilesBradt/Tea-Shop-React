01438c06c571316d71fea07a79e7dab18f02a26e
MilesBradt <smbradtmichael@gmail.com>
2020-04-25 21:27:45 -0700

    add key id

--- src/components/Home.js
+++ src/components/Home.js
@@ -14,7 +14,7 @@ function Home(props) {
             )} */}
             <h1>Green Tea</h1>
             {props.greenTea.map((item, index) =>
-            <p>
+            <p key={index}>
                 Name: {item.name}
                 <br />
                 Price: {item.price}
@@ -26,7 +26,7 @@ function Home(props) {
             )}
             <h1>Black Tea</h1>
             {props.blackTea.map((item, index) =>
-            <p>
+            <p key={index}>
                 Name: {item.name}
                 <br />
                 Price: {item.price}
@@ -38,7 +38,7 @@ function Home(props) {
             )}
             <h1>Oolong Tea</h1>
             {props.oolongTea.map((item, index) =>
-            <p>
+            <p key={index}>
                 Name: {item.name}
                 <br />
                 Price: {item.price}
@@ -50,7 +50,7 @@ function Home(props) {
             )}
             <h1>Herbal Tea</h1>
             {props.herbalTea.map((item, index) =>
-            <p>
+            <p key={index}>
                 Name: {item.name}
                 <br />
                 Price: {item.price}
