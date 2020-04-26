4e3d991906403af77915ab257f4d8268bced7601
MilesBradt <smbradtmichael@gmail.com>
2020-04-25 19:23:19 -0700

    create mock inventory

---
+++ src/assets/javascript/inventory.js
@@ -0,0 +1,108 @@
+const teaInventory = [
+    {
+        type: "Green Tea",
+        name: "Dragon Well",
+        price: 2.25,
+        stock: 100,
+        caffeine: "medium",
+        id: null
+    },
+    {
+        type: "Green Tea",
+        name: "Matcha",
+        price: 3.50,
+        stock: 50,
+        caffeine: "medium",
+        id: null
+    },
+    {
+        type: "Green Tea",
+        name: "Biluochun",
+        price: 2.75,
+        stock: 75,
+        caffeine: "medium",
+        id: null
+    },
+    {
+        type: "Green Tea",
+        name: "Gyokuro",
+        price: 2.25,
+        stock: 100,
+        caffeine: "medium",
+        id: null
+    },
+    {
+        type: "Black Tea",
+        name: "Earl Grey",
+        price: 2.25,
+        stock: 100,
+        caffeine: "high",
+        id: null
+    },
+    {
+        type: "Black Tea",
+        name: "English Breakfast",
+        price: 2.25,
+        stock: 80,
+        caffeine: "high",
+        id: null
+    },
+    {
+        type: "Black Tea",
+        name: "Darjeeling",
+        price: 2.75,
+        stock: 90,
+        caffeine: "high",
+        id: null
+    },
+    {
+        type: "Black Tea",
+        name: "Yunnan",
+        price: 3.25,
+        stock: 65,
+        caffeine: "high",
+        id: null
+    },
+    {
+        type: "Oolong",
+        name: "Baozhong",
+        price: 3.25,
+        stock: 70,
+        caffeine: "low",
+        id: null
+    },
+    {
+        type: "Oolong",
+        name: "Tie Guan Yin",
+        price: 2.75,
+        stock: 80,
+        caffeine: "low",
+        id: null
+    },
+    {
+        type: "Herbal",
+        name: "Chamomile",
+        price: 2.50,
+        stock: 100,
+        caffeine: "none",
+        id: null
+    },
+    {
+        type: "Herbal",
+        name: "Ginger",
+        price: 2.50,
+        stock: 100,
+        caffeine: "none",
+        id: null
+    },
+    {
+        type: "Herbal",
+        name: "Mint",
+        price: 2.50,
+        stock: 80,
+        caffeine: "none",
+        id: null
+    }
+
+]
+
